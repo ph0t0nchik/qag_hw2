4d4452fe9d5c822659b6b4a9bd2a6a9c5ed1c74c
Aleksei Maksimov <maximov0603@gmail.com>
2023-12-21 09:21:45 +0300

    add calculator test

--- src/test/java/com/demoqa/DemoJUnitTest.java
+++ src/test/java/com/demoqa/DemoJUnitTest.java
@@ -1,2 +1,45 @@
-package com.demoqa;public class DemoJUnitTest {
+package com.demoqa;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.*;
+
+public class DemoJUnitTest {
+
+    Calculator calc;
+    @BeforeAll
+    static void beforAll(){
+        System.out.println("#### @BeforeAll");
+//        Configuration.browser = "chrome";
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("#### @AfterAll");
+//        Configuration.browser = "chrome";
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("    #### @BeforeEach");
+        calc = new Calculator();
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("    #### @AfterEach");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("        #### @Test firstTest");
+        int result = calc.sum(2, 2);
+        Assertions.assertTrue(result == 4);
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("        #### @Test secondTest");
+        int result = calc.mult(2, 2);
+        Assertions.assertTrue(result == 4);
+    }
 }
